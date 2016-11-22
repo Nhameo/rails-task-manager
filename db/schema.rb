1f009dbbb44f7e9b44eda423fd6ad63bab5ebb16
Nhameo Benhebri <nhameo@gmail.com>
2016-11-22 18:28:52 +0100

    done not working, a rendre plus joli

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122112057) do
+ActiveRecord::Schema.define(version: 20161122172507) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "done",        default: false
+    t.string   "owner"
   end
 
 end
